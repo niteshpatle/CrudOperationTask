92b1d93b6bd0e2608e9c3f904c404ec1d278da25
nitesh <nitesh.patle@pratititech.com>
2023-07-05 16:44:26 +0530

    project commited

---
+++ CrudOperationApp/src/main/java/com/saksoft/app/CrudOperationAppApplication.java
@@ -0,0 +1,13 @@
+package com.saksoft.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperationAppApplication.class, args);
+	}
+
+}
---
+++ CrudOperationApp/src/test/java/com/saksoft/app/CrudOperationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saksoft.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
